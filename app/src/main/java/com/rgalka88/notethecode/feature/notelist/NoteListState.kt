24dc3dc8c011cb350ea7e0594f8c2be247db1199
Rafał Gałka <rafal.galka@grupawp.pl>
2020-02-22 12:30:04 +0100

    refactor layouts

--- app/src/main/java/com/rgalka88/notethecode/feature/notelist/NoteListState.kt
+++ app/src/main/java/com/rgalka88/notethecode/feature/notelist/NoteListState.kt
@@ -2,4 +2,4 @@ package com.rgalka88.notethecode.feature.notelist
 
 import com.airbnb.mvrx.MvRxState
 
-data class NoteListState(val text: String = "Hello World") : MvRxState
+data class NoteListState(val text: String = "Hello World Hello World Hello World Hello World") : MvRxState
